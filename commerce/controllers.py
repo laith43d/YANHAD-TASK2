5d2d9e8b13a10d514a21db68d7de6ec619898ff5
Worood esam <91219550+Wroo11d@users.noreply.github.com>
2021-11-12 14:02:59 +0300

    Update controllers.py

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -37,12 +37,12 @@ def create_product(request, payload: ProductCreate):
     return 201, product
 
 
-@commerce_controller.put('product/{id}')
-def update_product(request,id,payload:ProductCreate):
-    updateProduct=get_object_or_404(Product,id=id)
-    for attr,value in payload.dict().items():
-        setattr(updateProduct, attr, value)       
-    updateProduct.save()  
+@commerce_controller.put('product/{id}', response={200: UpdateProduct})
+def update_product(request, id: UUID4, payload: UpdateProduct):
+    product = get_object_or_404(Product, id=id)
+    for attr, value in payload.dict().items():
+        setattr(product, attr, value)
+    product.save()  
 
 
 @commerce_controller.delete('product/{id}')
